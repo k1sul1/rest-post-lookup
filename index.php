ae8a4169036f322d1e9689a677c803b3c21c734f
Christian Nikkanen <christian.nikkanen@vincit.com>
2018-01-15 17:27:51 +0200

    Refactor a bit

--- index.php
+++ index.php
@@ -13,8 +13,6 @@ require_once "class.db.php";
 \k1sul1\db(); // Open the connection
 
 add_action("plugins_loaded", function() {
-
-
   register_activation_hook(__FILE__, function() {
     $db = \k1sul1\db();
     $query = $db->prepare("
@@ -39,6 +37,9 @@ add_action("plugins_loaded", function() {
 
     $db->exec("CREATE TABLE IF NOT EXISTS wp_rpl_permalinks_temp LIKE wp_rpl_permalinks");
     $db->exec("TRUNCATE TABLE wp_rpl_permalinks_temp");
+
+    // In order to make this actually fast, generate a string query with VALUES(?,?) VALUES(?,?)...
+    // Should be about 8 times faster.
 
     $insert = $db->prepare("INSERT INTO wp_rpl_permalinks_temp (object_id, permalink) VALUES(?, ?)");
     while ($row = $query->fetch()) {
@@ -70,39 +71,67 @@ add_action("plugins_loaded", function() {
     }
   });
 
+  // add_action("admin_bar_menu", function($bar) {
+    // $bar->add_node([
+      // "id" => "rpl_regen_permalinks",
+      // "title" => "Regenerate permalinks",
+      // "href" => "/"
+    // ]);
+  // });
+
   add_action("rest_api_init", function() {
 
     register_rest_route("rpl/v1", "lookup", [
       "methods" => "GET",
       "callback" => function() {
         $url = $_GET["url"];
+        $legacy = false;
 
-        if (true) {
+        $getResponse = function($id) {
+          $post = get_post($id);
+          $type = $post->post_type;
+          $ptypeObject = get_post_type_object($type);
+          $endpoint = !empty($ptypeObject->rest_base) ? $ptypeObject->rest_base : $type;
+
+          setup_postdata($post); // The response may be faster with this.
+
+          $req = new \WP_REST_Request("GET", "/wp/v2/{$endpoint}/$id");
+
+          // None of these work!
+          // can't get an answer: https://twitter.com/k1sul1/status/945950740106838016
+          // set_query_params should work according to this https://developer.wordpress.org/reference/classes/wp_rest_request/
+
+
+          // $req->set_param("_embed", 1);
+          // $req["_embed"] = true;
+          // $req->set_query_params([
+            // "_embed" => true,
+            // "embed" => true,
+            // "embedded" => true,
+            // "_embedded" => true,
+          // ]);
+
+          return rest_do_request($req);
+        };
+
+        if (!$legacy) {
+          // should use wpdb for this so a separate connection isn't needed
           $query = \k1sul1\db()->prepare("SELECT object_id FROM wp_rpl_permalinks WHERE permalink = ? LIMIT 1");
           $query->execute([$url]);
           $id = $query->fetchColumn(0);
 
+          error_log(print_r($_REQUEST, true));
+          error_log(print_r($_COOKIE, true));
+          // error_log(wp_get_current_user());;
+
+          error_log("check for authentication before giving a post that is private!");
+
           if ($id !== false) {
-            $post = get_post($id);
-            $type = $post->post_type;
-            $ptypeObject = get_post_type_object($type);
-            $endpoint = !empty($ptypeObject->rest_base) ? $ptypeObject->rest_base : $type;
-
-            setup_postdata($post); // The response may be faster with this.
-
-            $req = new \WP_REST_Request("GET", "/wp/v2/{$endpoint}/$id");
-
-            return rest_do_request($req);
-
-            return new \WP_REST_Response([
-              // "id" => $query->fetch()["object_id"]
-              "post" => get_post($id),
-            ]);
+            return $getResponse($id);
           } else {
             $response =  new \WP_REST_Response([
               "error" => "No post found.",
             ]);
-            // $response->set_status(404); // Yes. Need to adjust axios
 
             return $response;
           }
@@ -113,17 +142,15 @@ add_action("plugins_loaded", function() {
             $response =  new \WP_REST_Response([
               "error" => "No post found.",
             ]);
-            // $response->set_status(404); // Yes. Need to adjust axios
 
             return $response;
           }
 
 
-          return new \WP_REST_Response([
-            "post" => get_post($id),
-          ]);
+          return $getResponse($id);
         }
       },
     ]);
   });
 });
+
